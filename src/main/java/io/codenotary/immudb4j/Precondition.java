29d8eab4054ecd6119850e316a5e1c619095e7e1
Artiom Iurcovschi <artiom.08@gmail.com>
2026-07-12 18:40:17 +0400

    Update precondtion to support utility class for convertion to bytes

--- src/main/java/io/codenotary/immudb4j/Precondition.java
+++ src/main/java/io/codenotary/immudb4j/Precondition.java
@@ -1,6 +1,20 @@
+/*
+Copyright 2022 CodeNotary, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package io.codenotary.immudb4j;
 
-import com.google.protobuf.ByteString;
 import io.codenotary.immudb.ImmudbProto;
 
 public interface Precondition {
@@ -33,7 +47,7 @@ public interface Precondition {
         public ImmudbProto.Precondition toProto() {
             return ImmudbProto.Precondition.newBuilder()
                     .setKeyMustExist(ImmudbProto.Precondition.KeyMustExistPrecondition.newBuilder()
-                            .setKey(ByteString.copyFrom(this.key)))
+                            .setKey(Utils.toByteString(this.key)))
                     .build();
         }
     }
@@ -62,7 +76,7 @@ public interface Precondition {
         public ImmudbProto.Precondition toProto() {
             return ImmudbProto.Precondition.newBuilder()
                     .setKeyMustNotExist(ImmudbProto.Precondition.KeyMustNotExistPrecondition.newBuilder()
-                            .setKey(ByteString.copyFrom(this.key)))
+                            .setKey(Utils.toByteString(this.key)))
                     .build();
         }
     }
@@ -98,7 +112,7 @@ public interface Precondition {
         public ImmudbProto.Precondition toProto() {
             return ImmudbProto.Precondition.newBuilder()
                     .setKeyNotModifiedAfterTX(ImmudbProto.Precondition.KeyNotModifiedAfterTXPrecondition.newBuilder()
-                            .setKey(ByteString.copyFrom(this.key))
+                            .setKey(Utils.toByteString(this.key))
                             .setTxID(this.txID)
                     )
                     .build();
